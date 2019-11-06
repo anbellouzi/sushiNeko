1282b362634b8b1aeab56356c389d437ac6bf912
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-06 12:48:55 -0800

    finished page 3 of tutorial

---
+++ Sushi Neko/SushiPiece.swift
@@ -0,0 +1,48 @@
+import SpriteKit
+
+class SushiPiece: SKSpriteNode {
+    
+    /* Chopsticks objects */
+    var rightChopstick: SKSpriteNode!
+    var leftChopstick: SKSpriteNode!
+    
+    /* Sushi type */
+    var side: Side = .none {
+        didSet {
+            switch side {
+            case .left:
+                /* Show left chopstick */
+                leftChopstick.isHidden = false
+            case .right:
+                /* Show right chopstick */
+                rightChopstick.isHidden = false
+            case .none:
+                /* Hide all chopsticks */
+                leftChopstick.isHidden = true
+                rightChopstick.isHidden = true
+            }
+        }
+    }
+    
+    
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func connectChopsticks() {
+        /* Connect our child chopstick nodes */
+        rightChopstick = (childNode(withName: "rightChopstick") as! SKSpriteNode)
+        leftChopstick = (childNode(withName: "leftChopstick") as! SKSpriteNode)
+        
+        /* Set the default side */
+        side = .none
+        
+    }
+    
+}
---
+++ Sushi Neko/character.swift
@@ -0,0 +1,36 @@
+//
+//  character.swift
+//  Sushi Neko
+//
+//  Created by Anas on 11/6/19.
+//  Copyright © 2019 Anas. All rights reserved.
+//
+
+import SpriteKit
+
+class Character: SKSpriteNode {
+    
+    /* Character side */
+    var side: Side = .left {
+        didSet {
+            if side == .left {
+                xScale = 1
+                position.x = 70
+            } else {
+                /* An easy way to flip an asset horizontally is to invert the X-axis scale */
+                xScale = -1
+                position.x = 252
+            }
+        }
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
