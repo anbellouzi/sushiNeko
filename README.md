ef14b3b843a5d46ad54e4f745f2dce4f0e107d7d
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-06 17:38:28 -0800

    Added Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+Autor: Anas Bellouzi
+Date: 11/06/2019
+
+Project Game: Sushi Neko
+
+Description: Build Sushi Neko -- a clone of the popular mobile game Timberman. We used Apple's Swift programming language together with SpriteKit, and game development framework.
+
+Resources:
+  - xCode
--- Sushi Neko/GameScene.swift
+++ Sushi Neko/GameScene.swift
@@ -11,6 +11,9 @@ class GameScene: SKScene {
     var sushiBasePiece: SushiPiece!
     /* Cat Character */
     var character: Character!
+    /* Sushi tower array */
+    var sushiTower: [SushiPiece] = []
+
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -22,6 +25,74 @@ class GameScene: SKScene {
         
         /* Setup chopstick connections */
         sushiBasePiece.connectChopsticks()
+        
+        /* Manually stack the start of the tower */
+        addTowerPiece(side: .none)
+        addTowerPiece(side: .right)
+    }
+    
+    func addTowerPiece(side: Side) {
+        /* Add a new sushi piece to the sushi tower */
+        
+        /* Copy original sushi piece */
+        let newPiece = sushiBasePiece.copy() as! SushiPiece
+        newPiece.connectChopsticks()
+        
+        /* Access last piece properties */
+        let lastPiece = sushiTower.last
+        
+        /* Add on top of last piece, default on first piece */
+        let lastPosition = lastPiece?.position ?? sushiBasePiece.position
+        newPiece.position.x = lastPosition.x
+        newPiece.position.y = lastPosition.y + 55
+        
+        /* Increment Z to ensure it's on top of the last piece, default on first piece*/
+        let lastZPosition = lastPiece?.zPosition ?? sushiBasePiece.zPosition
+        newPiece.zPosition = lastZPosition + 1
+        
+        /* Set side */
+        newPiece.side = side
+        
+        /* Add sushi to scene */
+        addChild(newPiece)
+        
+        /* Add sushi piece to the sushi tower */
+        sushiTower.append(newPiece)
+        
+        /* Randomize tower to just outside of the screen */
+        addRandomPieces(total: 5)
+    }
+    
+    
+    
+    func addRandomPieces(total: Int) {
+        /* Add random sushi pieces to the sushi tower */
+        
+        for _ in 1...total {
+            
+            /* Need to access last piece properties */
+            let lastPiece = sushiTower.last!
+            
+            /* Need to ensure we don't create impossible sushi structures */
+            if lastPiece.side != .none {
+                addTowerPiece(side: .none)
+            } else {
+                
+                /* Random Number Generator */
+                let rand = arc4random_uniform(100)
+                
+                if rand < 45 {
+                    /* 45% Chance of a left piece */
+                    addTowerPiece(side: .left)
+                } else if rand < 90 {
+                    /* 45% Chance of a right piece */
+                    addTowerPiece(side: .right)
+                } else {
+                    /* 10% Chance of an empty piece */
+                    addTowerPiece(side: .none)
+                }
+            }
+        }
     }
     
 }
